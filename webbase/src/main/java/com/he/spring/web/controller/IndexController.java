7c3c44d63985cab8fb1fb7cdfc864f649f66da4f
heyanjing <1366162208@qq.com>
2017-11-29 15:20:14 +0800

    语言本地化

--- webbase/src/main/java/com/he/spring/web/controller/IndexController.java
+++ webbase/src/main/java/com/he/spring/web/controller/IndexController.java
@@ -2,21 +2,18 @@ package com.he.spring.web.controller;
 
 import com.he.spring.bean.User;
 import com.he.spring.bean.User1;
-import org.apache.commons.codec.CharEncoding;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.propertyeditors.LocaleEditor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.support.RequestContextUtils;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
-import java.io.IOException;
 import java.util.Locale;
 
 /**
@@ -26,6 +23,10 @@ import java.util.Locale;
 public class IndexController {
     private static final Logger log = LoggerFactory.getLogger(IndexController.class);
 
+    @Autowired
+    CookieLocaleResolver localeResolver;
+
+
     @RequestMapping("/")
     public String index() {
         return "/index";
@@ -34,7 +35,6 @@ public class IndexController {
     @RequestMapping("/user")
     @ResponseBody
     public Object user(@Valid User1 user, BindingResult result) {
-
         if (result.hasErrors()) {
             log.error("{}", result.getAllErrors());
             log.error("{}", result.getFieldErrors());
@@ -42,33 +42,51 @@ public class IndexController {
         }
         return new User();
     }
-
+//    http://localhost:8080/spring/changelanguage?
+//    http://localhost:8080/spring/changelanguage?new_lang=en
     @RequestMapping(value = "/changelanguage")
-    public void changeLanguage(String new_lang, HttpServletRequest request, HttpServletResponse response) {
-        String msg = "";
-        new_lang="zh_CN";
-        try {
-            LocaleResolver localeResolver = RequestContextUtils.getLocaleResolver(request);
-            if (localeResolver == null) {
-                throw new IllegalStateException("No LocaleResolver found: not in a DispatcherServlet request?");
-            }
-
-            LocaleEditor localeEditor = new LocaleEditor();
-            localeEditor.setAsText(new_lang);
-            localeResolver.setLocale(request, response, (Locale) localeEditor.getValue());
-
-            msg = "Change Language Success!";
-        } catch (Exception ex) {
-            msg = "error";
-            ex.printStackTrace();
+    public void changeLanguage(String new_lang, HttpServletRequest request, HttpServletResponse response,Locale locale) {
+        log.info(locale.getCountry());
+        log.info(locale.getDisplayCountry());
+        log.info(locale.getLanguage());
+        log.info(locale.getDisplayLanguage());
+        //region Description 方式二
+        new_lang = new_lang == null ? "zh_cn" : new_lang;
+        new_lang = new_lang.toLowerCase();
+        if (new_lang.equals("zh_cn")) {
+            localeResolver.setLocale(request, response, Locale.CHINA);
+        }  else {
+            localeResolver.setLocale(request, response, Locale.ENGLISH);
         }
+        //endregion
 
-        response.setCharacterEncoding(CharEncoding.UTF_8);
 
-        try {
-            response.getWriter().print(msg);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        //region Description 方式一
+//        String msg = "";
+//        new_lang = new_lang == null ? "zh_cn" : new_lang;
+//        try {
+//            LocaleResolver localeResolver = RequestContextUtils.getLocaleResolver(request);
+//            if (localeResolver == null) {
+//                throw new IllegalStateException("No LocaleResolver found: not in a DispatcherServlet request?");
+//            }
+//
+//            LocaleEditor localeEditor = new LocaleEditor();
+//            localeEditor.setAsText(new_lang);
+//            localeResolver.setLocale(request, response, (Locale) localeEditor.getValue());
+//
+//            msg = "Change Language Success!";
+//        } catch (Exception ex) {
+//            msg = "error";
+//            ex.printStackTrace();
+//        }
+//
+//        response.setCharacterEncoding(CharEncoding.UTF_8);
+//
+//        try {
+//            response.getWriter().print(msg);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+        //endregion
     }
 }
--- webbase/src/main/webapp/WEB-INF/view/index.jsp
+++ webbase/src/main/webapp/WEB-INF/view/index.jsp
@@ -1,5 +1,8 @@
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags" %>
 <html>
 <body>
 <h2>Hello World!</h2>
+    <spring:message code="welcome"></spring:message>
+    <spring:message code="user.name.null"></spring:message>
 </body>
 </html>
